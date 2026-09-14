366f584d887ef5a98dd39f4d3cf6a8e2f611ca0b
krasi556 <krasymirr@gmail.com>
2026-09-14 17:36:46 +0300

    home page button

--- DjangoFirstEx/settings.py
+++ DjangoFirstEx/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -28,8 +27,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 ALLOWED_HOSTS = []
 
 import os
-
-DEBUG = os.getenv('DEBUG', 'False') == 'True'
 from pathlib import Path
 from dotenv import load_dotenv
 
@@ -49,7 +46,7 @@ PROJECT_APPS = [
     'people',
     'address',
     'qualifications',
-    'core'
+    'core',
 
 ]
 
--- core/urls.py
+++ core/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from core import views
 
+app_name = 'core'
 urlpatterns = [
     path('', views.show_homepage, name='homepage')
 ]
